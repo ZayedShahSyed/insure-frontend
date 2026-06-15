9aa4fb071c8008a843770bc5946f22af4f62fff7
Praneshhm25 <2494394@cognizant.com>
2026-06-15 11:52:01 +0530

    aded backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,5 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
-  apiUrl: 'https://insure-frontend.vercel.app/'
-=======
   apiUrl: 'https://insure-backend-2xzl.onrender.com'
->>>>>>> eb060c298491e02c90ad4cd5b4b651fa7d369b69
 };
 
